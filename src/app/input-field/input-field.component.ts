441ad72a53e91a47c3c1c6cbcbedc26b6aed895e
Kevin Buffington <kbuffington@gmail.com>
2018-09-03 11:32:13 -0500

    Basic handling of metadata in input-field, and display of all values

--- src/app/input-field/input-field.component.ts
+++ src/app/input-field/input-field.component.ts
@@ -1,24 +1,53 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { MetadataProperty } from '../services/track.service';
 
 @Component({
 	selector: 'input-field',
 	templateUrl: './input-field.component.html',
 	styleUrls: ['./input-field.component.scss']
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent implements OnChanges {
 	@Input() label: string;
-	@Input() value: string;
+	@Input() value: MetadataProperty;
 
 	@Output() valueChange = new EventEmitter();
 
-	constructor() { }
+	public different = false;
+	public showValues = false;
+	private alteredDefault = false;
 
-	ngOnInit() {
-		console.log(this.label, this.value);
+	constructor() {}
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.value && changes.value.currentValue) {
+			const val: MetadataProperty = changes.value.currentValue;
+			this.different = false;
+			this.showValues = false;
+			this.alteredDefault = false;
+			val.values.forEach(t => {
+				if (t !== val.default && val.default !== undefined) {
+					this.different = true;
+				}
+			});
+		}
 	}
 
-	valChanged(value) {
-		this.valueChange.emit(value);
+	defaultValChanged(value: string) {
+		this.value.default = value;
+		this.alteredDefault = true;	// doesn't check if default is actually different
+		this.valueChange.emit(this.value);
+	}
+
+	showValueList() {
+		setTimeout(() => {
+			this.showValues = true;
+		});
+	}
+
+	hideValueList() {
+		setTimeout(() => {
+			this.showValues = false;
+		});
 	}
 
 }
