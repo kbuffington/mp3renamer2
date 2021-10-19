82549f2549e81cd5a305bd7def8b14fc7e811896
Kevin Buffington <kbuffington@gmail.com>
2021-10-19 01:53:41 -0500

    Remove traces of fetch-interceptor

--- src/app/app-preload.service.ts
+++ src/app/app-preload.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CacheService } from '@services/cache.service';
-import { fetchCacheInterceptor, FetchInterceptorRegistry, fetchResponseInterceptor } from '@services/fetch-interceptor.service';
 
 @Injectable({ providedIn: 'root' })
 export class PreloadService {
-	constructor(/*private fetchInterceptorRegistry: FetchInterceptorRegistry,*/
-				private cacheService: CacheService) {}
+    constructor(private cacheService: CacheService) {}
 
-	public initializeApp() {
-		// this.fetchInterceptorRegistry.addInterceptor(fetchCacheInterceptor);
-		// this.fetchInterceptorRegistry.addInterceptor(fetchResponseInterceptor);
-		// this.fetchInterceptorRegistry.registerInterceptors();
-		// console.log('initialized');
-	}
+    public initializeApp() {
+        // console.log('initialized');
+    }
 }
 
 export function PreloadFactory(preloadService: PreloadService) {
-	return () => preloadService.initializeApp();
+    return () => preloadService.initializeApp();
 }
