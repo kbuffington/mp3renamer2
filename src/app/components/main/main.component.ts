27599637a06850006eba815a26e5a393bd35fa15
Kevin Buffington <kbuffington@gmail.com>
2021-10-19 13:12:14 -0500

    Cleanup

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -5,53 +5,53 @@ import { TrackService } from '@services/track.service';
 import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
-	selector: 'app-main',
-	templateUrl: './main.component.html',
-	styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit, OnDestroy {
-	public showArtist = true;
-	public tracks: any[] = [];
-	public trackSubscription: Subscription;
+    public showArtist = true;
+    public tracks: any[] = [];
+    public trackSubscription: Subscription;
 
-	constructor(private electronService: ElectronService, private ts: TrackService) {
-	}
+    constructor(private electronService: ElectronService, private ts: TrackService) {
+    }
 
-	ngOnInit() {
-		this.tracks = this.ts.getCurrentTracks();
-		this.trackSubscription = this.ts.getTracks().subscribe(tracks => this.tracks = tracks);
-	}
+    ngOnInit() {
+        this.tracks = this.ts.getCurrentTracks();
+        this.trackSubscription = this.ts.getTracks().subscribe(tracks => this.tracks = tracks);
+    }
 
-	ngOnDestroy() {
-		this.trackSubscription.unsubscribe();
-	}
+    ngOnDestroy() {
+        this.trackSubscription.unsubscribe();
+    }
 
-	previewRename() {
-		this.ts.previewFilenames('');
-	}
+    previewRename() {
+        this.ts.previewFilenames('');
+    }
 
-	revertRename() {
-		this.ts.revertFilenames();
-	}
+    revertRename() {
+        this.ts.revertFilenames();
+    }
 
-	setNames() {
-		console.log('TODO: setNames() in main.component');
-	}
+    setNames() {
+        console.log('TODO: setNames() in main.component');
+    }
 
-	renameFolder() {
-		console.log('TODO: renameFolder() in main.component');
-	}
+    renameFolder() {
+        console.log('TODO: renameFolder() in main.component');
+    }
 
-	getFanart() {
-		console.log('TODO: getFanart() in main.component');
-	}
+    getFanart() {
+        console.log('TODO: getFanart() in main.component');
+    }
 
-	downloadArt() {
-		console.log('TODO: downloadArt() in main.component');
-	}
+    downloadArt() {
+        console.log('TODO: downloadArt() in main.component');
+    }
 
-	quitApp() {
-		const mainProcess = this.electronService.remote.require('./main.js');
-		mainProcess.quitApp();
-	}
+    quitApp() {
+        const mainProcess = this.electronService.remote.require('./main.js');
+        mainProcess.quitApp();
+    }
 }
