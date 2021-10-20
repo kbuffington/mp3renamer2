2e62c6a9c77de471d80a7116788f42646921bb21
Kevin Buffington <kbuffington@gmail.com>
2021-10-20 01:39:14 -0500

    Cleanup

--- src/app/components/get-metadata/get-metadata.component.ts
+++ src/app/components/get-metadata/get-metadata.component.ts
@@ -5,84 +5,83 @@ import { MetadataObj, TrackService } from '@services/track.service';
 import { throwError as observableThrowError } from 'rxjs';
 
 export class ReleaseDisplay extends Release {
-	constructor (json: any, metadata: MetadataObj) {
-		super(json);
-		const partOfSet = metadata.partOfSet;
-		metadata.trackNumber.values.forEach((trackNumber: string, index) => {
-			const pos = (partOfSet.changed ? partOfSet.default : partOfSet.values[index]).split('/');
-			const disc = !!parseInt(pos[0]) ? parseInt(pos[0]) : 1;
-			const discTrackStr = `${disc}-${parseInt(trackNumber)}`;
-			const t = this.tracks.find(track => track.discTrackStr === discTrackStr);
-			if (t) {
-				t.metadataFound = true;
-				t.metadataTitle = metadata.title.values[index];
-				t.metadataDiffers = t.title !== t.metadataTitle;
-			}
-		});
-	}
+    constructor(json: any, metadata: MetadataObj) {
+        super(json);
+        const partOfSet = metadata.partOfSet;
+        metadata.trackNumber.values.forEach((trackNumber: string, index) => {
+            const pos = (partOfSet.changed ? partOfSet.default : partOfSet.values[index]).split('/');
+            const disc = parseInt(pos[0]) ? parseInt(pos[0]) : 1;
+            const discTrackStr = `${disc}-${parseInt(trackNumber)}`;
+            const t = this.tracks.find(track => track.discTrackStr === discTrackStr);
+            if (t) {
+                t.metadataFound = true;
+                t.metadataTitle = metadata.title.values[index];
+                t.metadataDiffers = t.title !== t.metadataTitle;
+            }
+        });
+    }
 }
 
 @Component({
-	selector: 'get-metadata',
-	templateUrl: './get-metadata.component.html',
-	styleUrls: ['./get-metadata.component.scss']
+    selector: 'get-metadata',
+    templateUrl: './get-metadata.component.html',
+    styleUrls: ['./get-metadata.component.scss'],
 })
 export class GetMetadataComponent implements OnInit {
-	public releaseData: any;
-	public releases: Release[] = [];
-	public artist: string = '';
-	public album: string = '';
-	public fetchingReleases = false;
-	public numTracks: number;
-	public selectedRelease: ReleaseDisplay;
+    public releaseData: any;
+    public releases: Release[] = [];
+    public artist = '';
+    public album = '';
+    public fetchingReleases = false;
+    public numTracks: number;
+    public selectedRelease: ReleaseDisplay;
 
-	private metadata: MetadataObj;
+    private metadata: MetadataObj;
 
-	constructor(private mb: MusicbrainzService,
-				private ts: TrackService) {}
+    constructor(private mb: MusicbrainzService,
+                private ts: TrackService) {}
 
-	ngOnInit() {
-		this.metadata = this.ts.getCurrentMetadata();
-		this.artist = this.metadata.artist.default;
-		this.album = this.metadata.album.default;
-		this.numTracks = this.ts.getNumTracks();
-		const artistMBID = this.metadata.MUSICBRAINZ_ARTISTID.default;
-		this.requestMetadata();
-	}
+    ngOnInit() {
+        this.metadata = this.ts.getCurrentMetadata();
+        this.artist = this.metadata.artist.default;
+        this.album = this.metadata.album.default;
+        this.numTracks = this.ts.getNumTracks();
+        this.requestMetadata();
+    }
 
-	public requestMetadata() {
-		this.selectedRelease = null;
-		this.fetchingReleases = true;
-		this.mb.searchReleases({ artist: this.artist, release: this.album })
-			.subscribe(
-				(data: any) => {
-					this.fetchingReleases = false;
-					this.releaseData = data;
-					this.releases = data.releases?.map(r => new Release(r)) ?? [];
-				},
-				error => this.handleError(error));
-	}
+    public requestMetadata() {
+        this.selectedRelease = null;
+        this.fetchingReleases = true;
+        this.mb.searchReleases({ artist: this.artist, release: this.album })
+            .subscribe(
+                (data: any) => {
+                    this.fetchingReleases = false;
+                    this.releaseData = data;
+                    this.releases = data.releases?.map(r => new Release(r)) ?? [];
+                },
+                error => this.handleError(error));
+    }
 
-	public getReleaseInfo(release: Release) {
-		this.mb.getReleaseInfo(release.id)
-			.subscribe(
-				(release: any) => {
-					this.selectedRelease = new ReleaseDisplay(release, this.metadata);
-					console.log(this.selectedRelease);
-				},
-				error => this.handleError(error));
-	}
+    public getReleaseInfo(release: Release) {
+        this.mb.getReleaseInfo(release.id)
+            .subscribe(
+                (release: any) => {
+                    this.selectedRelease = new ReleaseDisplay(release, this.metadata);
+                    console.log(this.selectedRelease);
+                },
+                error => this.handleError(error));
+    }
 
-	private handleError(error: any) {
-		// In a real world app, we might use a remote logging infrastructure
-		// We'd also dig deeper into the error to get a better message
-		const errMsg = (error.message) ? error.message :
-			error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-		console.error(errMsg); // log to console instead
-		return observableThrowError(errMsg);
-	}
+    private handleError(error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        const errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return observableThrowError(errMsg);
+    }
 
-	public apply() {
+    public apply() {
 
-	}
+    }
 }
--- src/app/dont-allow-on-reload.guard.ts
+++ src/app/dont-allow-on-reload.guard.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
 export class DontAllowOnReload implements CanActivate {
-	private router: Router;
+    private router: Router;
 
-	// I initialize the secondary-view route guard.
-	constructor( router: Router ) {
-		this.router = router;
-	}
+    // I initialize the secondary-view route guard.
+    constructor(router: Router) {
+        this.router = router;
+    }
 
-    public canActivate(activatedRouteSnapshot: ActivatedRouteSnapshot,
-                        routerStateSnapshot: RouterStateSnapshot): boolean {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public canActivate(activatedRouteSnapshot: ActivatedRouteSnapshot, routerStateSnapshot: RouterStateSnapshot): boolean {
         if (this.isPageRefresh()) {
             this.router.navigate(['/']);
             return false;
