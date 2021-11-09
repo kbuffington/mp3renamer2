1197e25281381e141fdacc77c274df0da3eefb26
Kevin Buffington <kbuffington@gmail.com>
2021-11-09 17:43:32 -0600

    Cleanup

--- src/app/components/unknown-properties/unknown-properties.component.ts
+++ src/app/components/unknown-properties/unknown-properties.component.ts
@@ -2,50 +2,50 @@ import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@
 import { TrackService, UnknownPropertiesObj } from '@services/track.service';
 
 @Component({
-	selector: 'unknown-properties',
-	templateUrl: './unknown-properties.component.html',
-	styleUrls: ['./unknown-properties.component.scss']
+    selector: 'unknown-properties',
+    templateUrl: './unknown-properties.component.html',
+    styleUrls: ['./unknown-properties.component.scss'],
 })
 export class UnknownPropertiesComponent implements OnInit, OnDestroy, OnChanges {
-	@Input() unknownProperties: UnknownPropertiesObj;
+    @Input() unknownProperties: UnknownPropertiesObj;
 
-	unknownPropArray = [];
-	public editing: boolean[] = [];
-	public selected: any[] = [];
+    unknownPropArray = [];
+    public editing: boolean[] = [];
+    public selected: any[] = [];
 
-	constructor(private ts: TrackService) {}
+    constructor(private ts: TrackService) {}
 
-	ngOnInit() {}
+    ngOnInit() {}
 
-	ngOnDestroy() {}
+    ngOnDestroy() {}
 
-	ngOnChanges(changes: SimpleChanges) {
-		if (changes.unknownProperties && changes.unknownProperties.currentValue) {
-			this.unknownPropArray = Object.keys(changes.unknownProperties.currentValue);
-			// this.selected = changes.unknownProperties.currentValue.map(t => t);
-			this.clearEditing();
-		}
-	}
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.unknownProperties && changes.unknownProperties.currentValue) {
+            this.unknownPropArray = Object.keys(changes.unknownProperties.currentValue);
+            // this.selected = changes.unknownProperties.currentValue.map(t => t);
+            this.clearEditing();
+        }
+    }
 
-	clearEditing() {
-		const numRows = this.unknownPropArray.length + 1;
-		const numCols = 2;
-		const editingGrid = [];
-		for (let i = 0; i < numRows; i++) {
-			editingGrid[i] = [];
-			for (let j = 0; j < numCols; j++) {
-				editingGrid[i][j] = false;
-			}
-		}
-		this.editing = editingGrid;
-	}
+    clearEditing() {
+        const numRows = this.unknownPropArray.length + 1;
+        const numCols = 2;
+        const editingGrid = [];
+        for (let i = 0; i < numRows; i++) {
+            editingGrid[i] = [];
+            for (let j = 0; j < numCols; j++) {
+                editingGrid[i][j] = false;
+            }
+        }
+        this.editing = editingGrid;
+    }
 
-	startEditing(index: number, col: number) {
-		this.clearEditing();
-		this.editing[index][col] = true;
-	}
+    startEditing(index: number, col: number) {
+        this.clearEditing();
+        this.editing[index][col] = true;
+    }
 
-	selectionChanged(selection: any) {
-		console.log(selection);
-	}
+    selectionChanged(selection: any) {
+        console.log(selection);
+    }
 }
